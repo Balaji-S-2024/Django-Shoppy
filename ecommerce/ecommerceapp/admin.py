9060d24fb3324930d02e7787d74006d7d3e0e8c3
Balaji-S-2024 <balajisankar0202@gmail.com>
2023-08-11 10:52:28 +0530

    before paytm integration

--- ecommerce/ecommerceapp/admin.py
+++ ecommerce/ecommerceapp/admin.py
@@ -4,3 +4,5 @@ from ecommerceapp import models
 # Register your models here.
 admin.site.register(models.Contact)
 admin.site.register(models.Product)
+admin.site.register(models.Order)
+admin.site.register(models.OrderUpdate)
--- ecommerce/ecommerceapp/views.py
+++ ecommerce/ecommerceapp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from ecommerceapp.models import Contact, Product
+from django.shortcuts import render, redirect
+from ecommerceapp.models import Contact, Product, Order, OrderUpdate
 from django.contrib import messages
 from math import ceil
 
@@ -35,5 +35,58 @@ def contact(request):
     return render(request, "contact.html")
 
 def checkout(request):
+    if request.user.is_authenticated == False:
+        messages.warning(request, "Login and Try Again")
+        return redirect('/authent/login/')
+    
+    if request.method == 'POST':
+        items_json = request.POST.get('itemsJson', '')
+        amont = request.POST.get('amt')
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        address1 = request.POST.get('address1')
+        address2 = request.POST.get('address2')
+        city = request.POST.get('city')
+        state = request.POST.get('state')
+        phone = request.POST.get('phone')
+        zip_code = request.POST.get('zip_code')
+
+        order = Order(
+            items_json = items_json, 
+            name = name, 
+            amount = amont, 
+            email = email, 
+            address1 = address1, 
+            address2 = address2,
+            city = city, 
+            state = state,
+            zip_code = zip_code, 
+            phone = phone, 
+            )
+        print(amont)
+        Order.save()
+        update = OrderUpdate(order_id = Order.order_id, update_desc = "The order is Placed! :)")
+        update.save()
+        thank = True
+
+
+        # PayTm Integration
+        id = Order.order_id
+        oid=str(id)+"ShopyCart"
+        param_dict = {
+
+            'MID':keys.MID,
+            'ORDER_ID': oid,
+            'TXN_AMOUNT': str(amont),
+            'CUST_ID': email,
+            'INDUSTRY_TYPE_ID': 'Retail',
+            'WEBSITE': 'WEBSTAGING',
+            'CHANNEL_ID': 'WEB',
+            'CALLBACK_URL': 'http://127.0.0.1:8000/handlerequest/',
+
+        }
+        
+
+        
 
     return render(request, "checkout.html")
